fe84c6b648faf0eca327ec242ea9aca1b5ecda58
Gagik Arustamyan <g.arustamyan@gmail.com>
2020-01-24 16:00:25 +0400

    Add typescript support

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 module.exports = {
 
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ["@typescript-eslint"],
+
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true
+    }
+  },
+
   rules: {
       semi: [2, "always"]
   }
